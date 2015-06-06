ccf29e7c90962734e3c194ba6f1ffa7a37fd1e71
Arandi López <arandilopez.93@gmail.com>
2015-06-06 10:34:34 -0500

    sobre escritura del metodo

--- src/Providers/LumenFeedServiceProvider.php
+++ src/Providers/LumenFeedServiceProvider.php
@@ -1,5 +1,19 @@
 <?php namespace ArandiLopez\Feed\Providers\LumenFeedServiceProvider;
 
-class LumenFeedServiceProvider extends FeedServiceProvider {
+use ArandiLopez\Feed\Factories\FeedFactory;
+use ArandiLopez\Feed\Providers\FeedServiceProvider;
 
+class LumenFeedServiceProvider extends FeedServiceProvider {
+    public function boot(){}
+
+    public function registerFeedFactory()
+    {
+        $config = include __DIR__ . '/../config/feed.php';
+        $this->app->bindShared('feed', function () {
+            if (!$config) {
+                throw new \RunTimeException('Feed Parser configuration not found.');
+            }
+            return new FeedFactory($config);
+        });
+    }
 }
